2f9dbf3bf8d011d41884b0d2f365a1db7bd9710c
Joseba <jhm0025@alu.ubu.es>
2018-05-12 12:49:50 +0200

    11- Logging
    Refactorizado codigo con extract method. Y tests pasados correctamente.

--- src/lanSimulation/Network.java
+++ src/lanSimulation/Network.java
@@ -225,10 +225,7 @@ public class Network {
 				report.write("\tNode '");
 				report.write(currentNode.name_);
 				report.write("' accepts broadcase packet.\n");
-				report.write("\tNode '");
-				report.write(currentNode.name_);
-				report.write("' passes packet on.\n");
-				report.flush();
+				node(report, currentNode);
 			} catch (IOException exc) {
 				// just ignore
 			}
@@ -291,10 +288,7 @@ public class Network {
 		startNode = (Node) workstations_.get(workstation);
 
 		try {
-			report.write("\tNode '");
-			report.write(startNode.name_);
-			report.write("' passes packet on.\n");
-			report.flush();
+			node(report, startNode);
 		} catch (IOException exc) {
 			// just ignore
 		}
@@ -304,10 +298,7 @@ public class Network {
 		while ((!packet.destination_.equals(currentNode.name_))
 				& (!packet.origin_.equals(currentNode.name_))) {
 			try {
-				report.write("\tNode '");
-				report.write(currentNode.name_);
-				report.write("' passes packet on.\n");
-				report.flush();
+				node(report, currentNode);
 			} catch (IOException exc) {
 				// just ignore
 			}
@@ -330,6 +321,13 @@ public class Network {
 		}
 
 		return result;
+	}
+
+	private void node(Writer report, Node currentNode) throws IOException {
+		report.write("\tNode '");
+		report.write(currentNode.name_);
+		report.write("' passes packet on.\n");
+		report.flush();
 	}
 
 	private boolean printDocument(Node printer, Packet document, Writer report) {
