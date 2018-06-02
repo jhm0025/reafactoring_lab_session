9405551ff5602878eab31debc9c492b8c6365e50
DanielHernando <dhb0005@alu.ubu.es>
2018-06-02 19:42:06 +0200

    versioado bueno
    version anterior

--- src/lanSimulation/internals/Printer.java
+++ src/lanSimulation/internals/Printer.java
@@ -6,33 +6,6 @@ public class Printer extends Node{
 		super(type, name, nextNode);
 	}
 	
-	public static void printXMLOnPrinter(StringBuffer buf) {
-		buf.append("<printer>");
-		buf.append(name_);
-		buf.append("</printer>");
-	}
 
-	
-
-	public static void printXMLOnNode(StringBuffer buf) {
-		buf.append("<node>");
-		buf.append(name_);
-		buf.append("</node>");
-	}
-	
-	
-	public static void printOnPrinter(StringBuffer buf) {
-		buf.append("Printer ");
-		buf.append(name_);
-		buf.append(" [Printer]");
-	}
-
-	
-
-	public static void printOnNode(StringBuffer buf) {
-		buf.append("Node ");
-		buf.append(name_);
-		buf.append(" [Node]");
-	}
 
 }
--- src/lanSimulation/internals/Workstation.java
+++ src/lanSimulation/internals/Workstation.java
@@ -5,16 +5,5 @@ public class Workstation extends Node{
 	public Workstation(byte type,String name,Node nextNode) {
 		super(type,name ,nextNode);
 	}
-	public static void printOnWorkstation(StringBuffer buf) {
-		buf.append("Workstation ");
-		buf.append(name_);
-		buf.append(" [Workstation]");
-	}
-	
-	
-	public static void printXMLOnWorkstation(StringBuffer buf) {
-		buf.append("<workstation>");
-		buf.append(name_);
-		buf.append("</workstation>");
-	}
+
 }
