d0bd0938ede4f1de0988f9d5aaf290a191b1d153
Joseba <jhm0025@alu.ubu.es>
2018-06-02 20:03:58 +0200

    21. Codigo movido de clase "Node" a clase "Printer" y "Workstation"
    Codigo movido a las clases correspondientes. Todos test OK.

--- src/lanSimulation/internals/Printer.java
+++ src/lanSimulation/internals/Printer.java
@@ -6,6 +6,17 @@ public class Printer extends Node{
 		super(type, name, nextNode);
 	}
 	
+	public void printXMLOnPrinter(StringBuffer buf) {
+		buf.append("<printer>");
+		buf.append(name_);
+		buf.append("</printer>");
+	}
+	
+	public void printOnPrinter(StringBuffer buf) {
+		buf.append("Printer ");
+		buf.append(name_);
+		buf.append(" [Printer]");
+	}
 
 
 }
--- src/lanSimulation/internals/Workstation.java
+++ src/lanSimulation/internals/Workstation.java
@@ -5,5 +5,17 @@ public class Workstation extends Node{
 	public Workstation(byte type,String name,Node nextNode) {
 		super(type,name ,nextNode);
 	}
+	
+	public void printXMLOnWorkstation(StringBuffer buf) {
+		buf.append("<workstation>");
+		buf.append(name_);
+		buf.append("</workstation>");
+	}
+	
+	public void printOnWorkstation(StringBuffer buf) {
+		buf.append("Workstation ");
+		buf.append(name_);
+		buf.append(" [Workstation]");
+	}
 
 }
